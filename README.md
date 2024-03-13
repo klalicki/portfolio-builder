c870b35ea2bc4778e519fe6977810bfcec49abd5
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-03-12 22:55:43 -0400

    installs astro

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # portfolio-builder
 
 A portfolio builder/static site generator built with Keystatic CMS
+
+## Keystatic in Astro
+
+This template shows how you can use Keystatic in an Astro site.
+
+To setup:
+
+```bash
+npm install
+```
+
+To run:
+
+```
+npm run dev
+```
+
+Admin UI: [http://127.0.0.1:4321/keystatic](http://127.0.0.1:4321/keystatic)
+
+Homepage: [http://localhost:4321](http://localhost:4321)
---
+++ keystatic.config.ts
@@ -0,0 +1,28 @@
+import { config, fields, collection } from '@keystatic/core';
+
+export default config({
+  storage: {
+    kind: 'local',
+  },
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      slugField: 'title',
+      path: 'src/content/posts/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        content: fields.document({
+          label: 'Content',
+          formatting: true,
+          dividers: true,
+          links: true,
+          images: {
+            directory: 'src/assets/images/posts',
+            publicPath: '../../assets/images/posts/',
+          },
+        }),
+      },
+    }),
+  },
+});
