a4fa11ec913bc1b8cd7be11aaa877f2001707fd1
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-03-13 21:18:18 -0400

    adds code to astro config to allow for static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import markdoc from '@astrojs/markdoc';
-import keystatic from '@keystatic/astro';
+const isDev = process.env.NODE_ENV === "development";
+
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic()],
-  output: 'hybrid',
+  integrations: [react(), markdoc(), ...(isDev ? [keystatic()] : [])],
+  output: isDev ? "hybrid" : "static",
 });
