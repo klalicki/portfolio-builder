437b164b8a664267ad40a5d78bc9340e7d0c71ac
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-03-29 22:38:59 -0400

    continuing to refactor settings

--- cms/singletons/portfolioLayouts.ts
+++ cms/singletons/portfolioLayouts.ts
@@ -4,7 +4,7 @@ import {
   type BasicFormField,
   BlockWrapper,
 } from "@keystatic/core";
-import { portfolioComponents } from "../components/portfolioViewComponents";
+import { customFields } from "../fields";
 export const portfolioLayouts = singleton({
   label: "Portfolio Layout Settings",
   format: { data: "json" },
@@ -17,6 +17,11 @@ export const portfolioLayouts = singleton({
           description:
             "This can be either a number (1 is square, higher than 1 is a horizontal rectangle), or a ratio ie 3/2",
           defaultValue: "3/2",
+        }),
+        itemGap: customFields.cssUnit({
+          label: "Gap",
+          description: "The gap between projects in this layout",
+          defaultValue: "0px",
         }),
       },
       { label: "Side-by-Side layout" }
