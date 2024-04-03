d0419b6f9762bef0753933e6c5bc660b86b68afc
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-04-03 15:50:09 -0400

    implements some parameters for CoolTile layout

--- cms/singletons/portfolioLayouts.ts
+++ cms/singletons/portfolioLayouts.ts
@@ -5,11 +5,35 @@ import {
   BlockWrapper,
 } from "@keystatic/core";
 import { customFields } from "../fields";
+
 export const portfolioLayouts = singleton({
   label: "Portfolio Layout Settings",
   format: { data: "json" },
   path: "src/settings/portfolioLayouts",
   schema: {
+    coolTile: fields.object(
+      {
+        tileAspectRatio: fields.text({
+          label: "Tile Aspect Ratio",
+          description:
+            "This can be either a number (1 is square, higher than 1 is a horizontal rectangle), or a ratio ie 3/2",
+          defaultValue: "3/2",
+        }),
+        itemGap: customFields.cssUnit({
+          label: "Gap",
+          description: "The gap between projects in this layout",
+          defaultValue: "0px",
+        }),
+        targetWidth: fields.number({
+          label: "Tile Width (px)",
+          description:
+            "The width the tiles will try to be if there is enough space",
+          validation: { isRequired: true, max: 1200, min: 0 },
+        }),
+      },
+
+      { label: "Cool Tile layout" }
+    ),
     sideBySide: fields.object(
       {
         tileAspectRatio: fields.text({
