bb8b945c0c7b8c63083a0ea1dde4536e3b4f3a0b
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-04-06 00:53:01 -0400

    moves typeOptions to be stored in JSON

--- cms/singletons/typeOptions.ts
+++ cms/singletons/typeOptions.ts
@@ -2,6 +2,8 @@ import { singleton } from "@keystatic/core";
 import { typeProps } from "../fields/fieldGroups/typeProps";
 export const typeOptions = singleton({
   label: "Typography Options",
+  format: { data: "json" },
+  path: "src/settings/typeOptions",
   schema: {
     h1: typeProps({ label: "Heading 1" }),
   },
