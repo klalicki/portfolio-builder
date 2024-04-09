a83bbd032596d26cfa41d69eaa3a692b5f65d174
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-04-09 14:35:39 -0400

    implements custom typography for tile layout stuff

--- cms/singletons/portfolioLayouts.ts
+++ cms/singletons/portfolioLayouts.ts
@@ -5,6 +5,7 @@ import {
   BlockWrapper,
 } from "@keystatic/core";
 import { customFields } from "../fields";
+import { typeProps } from "../fields/fieldGroups/typeProps";
 
 export const portfolioLayouts = singleton({
   label: "Portfolio Layout Settings",
@@ -13,54 +14,69 @@ export const portfolioLayouts = singleton({
   schema: {
     coolTile: fields.object(
       {
-        containerWidth: customFields.cssUnit({
-          label: "Container Width",
-          description: "The maximum width the portfolio group can be",
-          defaultValue: "1000px",
-          isCompact: true,
+        container: fields.object({
+          width: customFields.cssUnit({
+            label: "Container Width",
+            description: "The maximum width the portfolio group can be",
+            defaultValue: "1000px",
+            isCompact: true,
+          }),
+          padding: customFields.cssUnit({
+            label: "Container Padding",
+            description: "The space around the edge of the portfolio grid",
+            defaultValue: "1rem",
+            limitUnits: ["px", "rem", "em"],
+            isCompact: true,
+          }),
+          gap: customFields.cssUnit({
+            label: "Gap",
+            description: "The gap between projects in this layout",
+            defaultValue: "0px",
+            limitUnits: ["px", "rem", "em"],
+            isCompact: true,
+          }),
         }),
-        containerPadding: customFields.cssUnit({
-          label: "Container Padding",
-          description: "The space around the edge of the portfolio grid",
-          defaultValue: "1rem",
-          limitUnits: ["px", "rem", "em"],
-          isCompact: true,
-        }),
-        itemGap: customFields.cssUnit({
-          label: "Gap",
-          description: "The gap between projects in this layout",
-          defaultValue: "0px",
-          limitUnits: ["px", "rem", "em"],
-          isCompact: true,
-        }),
-        tileAspectRatio: fields.text({
-          label: "Tile Aspect Ratio",
-          description:
-            "This can be either a number (1 is square, higher than 1 is a horizontal rectangle), or a ratio ie 3/2",
-          defaultValue: "3/2",
+        tiles: fields.object({
+          aspectRatio: fields.text({
+            label: "Tile Aspect Ratio",
+            description:
+              "This can be either a number (1 is square, higher than 1 is a horizontal rectangle), or a ratio ie 3/2",
+            defaultValue: "3/2",
+          }),
+
+          // minTileWidth: customFields.cssFlex({ label: "Tile Width" }),
+          minWidth: fields.number({
+            label: "min tile Width (px)",
+            description: "The minimum width the tiles will be",
+            validation: { isRequired: true, max: 1200, min: 0 },
+          }),
+          overlayColor: customFields.colorPicker({
+            label: "Overlay Color",
+            defaultValue: "#000000",
+            compact: true,
+          }),
+          useProjectColor: fields.checkbox({
+            label: "Use Project Accent Color",
+          }),
+          overlayOpacity: fields.number({
+            label: "Overlay Opacity %",
+            validation: { min: 0, max: 100 },
+          }),
         }),
 
-        // minTileWidth: customFields.cssFlex({ label: "Tile Width" }),
-        minTileWidth: fields.number({
-          label: "min tile Width (px)",
-          description: "The minimum width the tiles will be",
-          validation: { isRequired: true, max: 1200, min: 0 },
-        }),
-        overlayColor: customFields.colorPicker({
-          label: "Overlay Color",
-          defaultValue: "#000000",
-          compact: true,
-        }),
-        useProjectAccentColors: fields.checkbox({
-          label: "Use Project Accent Color",
-        }),
-        projectAccentColorOpacity: fields.number({
-          label: "Overlay Opacity %",
-          validation: { min: 0, max: 100 },
-        }),
+        typography: fields.object(
+          {
+            title: typeProps({ label: "Project Title" }),
+            line2: typeProps({ label: "Line 2" }),
+            line3: typeProps({ label: "Line 3" }),
+          },
+          { label: "Typography" }
+        ),
       },
 
-      { label: "Cool Tile layout", layout: [4, 4, 4, 3, 1, 4, 3, 1] }
+      {
+        label: "Cool Tile layout",
+      }
     ),
     sideBySide: fields.object(
       {
---
+++ src/components/helpers/fontRuleBuilder.tsx
@@ -0,0 +1,21 @@
+export const fontRules = (specs: {
+  [key: string]: { [key: string]: string };
+}) => {
+  const fontRules: { [key: string]: string } = {};
+
+  const fontSpecs = specs;
+  const fontSpecSets = Object.entries(fontSpecs);
+  fontSpecSets.forEach((pair) => {
+    const fontVars = Object.entries(pair[1]);
+    fontVars.forEach((fontVarPair) => {
+      const keyName = pair[0] + "-" + fontVarPair[0];
+      if (fontVarPair[0] === "fontFamily") {
+        fontRules[keyName] = `var(--${fontVarPair[1]})`;
+      } else {
+        // @ts-ignore
+        fontRules[keyName] = fontVarPair[1];
+      }
+    });
+  });
+  return fontRules;
+};
