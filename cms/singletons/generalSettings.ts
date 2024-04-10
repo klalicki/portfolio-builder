385d19958beedee339c3fd01b8913d33b4885e2f
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-04-10 12:07:46 -0400

    adds field for favicon and uploads initial favicon

--- cms/singletons/generalSettings.ts
+++ cms/singletons/generalSettings.ts
@@ -1,8 +1,14 @@
-import { singleton } from "@keystatic/core";
+import { fields, singleton } from "@keystatic/core";
 import * as customFields from "../fields";
 export const generalSettings = singleton({
   label: "General Settings",
   path: "src/settings/general",
   format: { data: "json" },
-  schema: { slug: customFields.slugPicker({ label: "sluggy" }) },
+  schema: {
+    favicon: fields.image({
+      label: "Favicon",
+      directory: "src/assets/images/general",
+      publicPath: "../../assets/images/general/",
+    }),
+  },
 });
